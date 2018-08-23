f0425870c2b106cb94ab5494316c6a2d6c94edc5
howellsuen <howell.suen@gmail.com>
2018-08-24 04:38:50 +0800

    remove duplicant files in root directory

--- backend_ce/seeds/initial-user.js
+++ backend_ce/seeds/initial-user.js
@@ -1,17 +1,17 @@
 exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex('users').del()
-        .then(function() {
-            // Inserts seed entries
-            return knex('users').insert([{
-                id: 1,
-                display_name: 'Test',
-                email: 'test@gmail.com',
-                password: '1234',
-                phone: '12345678',
-                propic_path: '',
-                balance: 1000,
-                role: 'student'
-            }]);
-        });
+	// Deletes ALL existing entries
+	return knex('users').del()
+		.then(function() {
+			// Inserts seed entries
+			return knex('users').insert([{
+				id: 1,
+				display_name: 'Test',
+				email: 'test@gmail.com',
+				password: '1234',
+				phone: '12345678',
+				propic_path: '',
+				balance: 1000,
+				role: 'student'
+			}]);
+		});
 };
