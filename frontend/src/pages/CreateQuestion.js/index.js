10e3fbf0b5cd240f6b07ffcef607d9aef88c6f67
Unknown <leahokaying@gmail.com>
2018-08-30 01:02:38 +0800

    i hate that react browser icon, so i change jor haha

---
+++ frontend/src/pages/CreateQuestion.js/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class AboutUs extends React.Component {
+  render() {
+    return (
+      <div
+        className="jumbotron jumbotron-fluid"
+        style={{ margin: 0, background: '#00B0AF' }}
+      >
+        <div className="container">
+          <h1 className="display-4">My Question</h1>
+          <a className="btn btn-primary " href="/">
+            Ask Question Now!
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+export default AboutUs;
