36729faabd6a7f8b6cf8fd7c252424a46c291c00
Unknown <christypacc21@gmail.com>
2018-08-30 15:08:04 +0800

    update backend routes (4+2+2)

--- backend_ce/routers/ForumRouter.js
+++ backend_ce/routers/ForumRouter.js
@@ -11,21 +11,56 @@ module.exports = class ForumRouter {
   router() {
     let router = express.Router();
     router.get('/posts', auth.authenticate(), this.getPosts.bind(this)); //get all posts
-    // router.post('/posts', auth.authenticate(), this.getPosts.bind(this)); //create(post) a new post
+    // router.post('/posts', auth.authenticate(), this.createPosts.bind(this)); //create(post) a new post
     // router.get('/posts/:id', auth.authenticate(),this.getPostDetails.bind(this)); //get individual posts and corresponding comments
     // router.delete('/posts/:id', auth.authenticate(),this.delPost.bind(this)); //delete individual post (won't delete comments)
 
-    // router.post('/comment', auth.authenticate(),this.getPosts.bind(this)); //create(post) a new comment
-    // router.delete('/posts/:id/comments/:comments_id', auth.authenticate(),this.getPosts.bind(this)); //delete a comment
+    // router.post('/comment', auth.authenticate(),this.getComments.bind(this)); //create(post) a new comment
+    // router.delete('/posts/:id/comments/:comments_id', auth.authenticate(),this.delComment.bind(this)); //delete a comment
+
+    // router.get('/myposts', auth.authenticate(),this.getMyPosts.bind(this)); //get myPosts
+    // router.get('/mycomments', auth.authenticate(),this.getMyComments.bind(this)); //get myComments
 
     return router;
   }
 
+  //----------four routes about forum posts----------
   getPosts(req, res) {
     // return this.forumService.getForumPosts(req.query.sort)
     return this.forumService
-      .getForumPosts()
+      .getPosts()
       .then(posts => res.json(posts))
       .catch(err => res.status(500).json(err));
+  }
+
+  createPosts(req, res) {
+    return this.forumService.createPosts();
+  }
+
+  getPostDetails(req, res) {
+    return this.forumService.getPostDetail();
+    // .then(postDetails => res.json(postDetails))
+    // .catch(err => res.status(500).json(err));
+  }
+
+  delPost(req, res) {
+    return this.forumService.delPost();
+  }
+
+  //----------two routes about forum's indiv posts' comments----------
+  postComments(req, res) {
+    return this.forumService.postComments();
+  }
+
+  delComments(req, res) {
+    return this.forumService.delComments();
+  }
+  //----------two routes about getting my post and my comments (with identifying user id)----------
+  getMyPosts(req, res) {
+    return this.forumService.getMyPosts();
+  }
+
+  getMyComments(req, res) {
+    return this.forumService.getMyComments();
   }
 };
--- backend_ce/services/ForumService.js
+++ backend_ce/services/ForumService.js
@@ -3,15 +3,53 @@ module.exports = class ForumService {
     this.knex = knex;
   }
 
-  getForumPosts() {
+  //----------four services about forum posts----------
+
+  getPosts() {
     // let sort = 'forumPosts.created_at';
     // if (req.query.sort != null) {
     //     sort = req.query.sort;
     //   }
     return this.knex
-      .select("*")
+      .select('*')
       .from('forumPosts')
-      .join('users', 'users.id', 'forumPosts.user_id')
+      .join('users', 'users.id', 'forumPosts.user_id');
     //.order
   }
+
+  createPost() {
+    return this.knex;
+  }
+
+  getPostDetails() {
+    return this.knex;
+  }
+
+  delPost() {
+    return this.knex;
+  }
+
+  //----------two services about forum's indiv posts' comments----------
+  postComments() {
+    return this.knex;
+  }
+
+  delComments() {
+    return this.knex;
+  }
+  //----------two services about getting my post and my comments (with identifying user id)----------
+  getMyPosts() {
+    return this.knex;
+  }
+  getMyComments() {
+    return this.knex;
+  }
 };
+
+// app.get('/myadvice', authHelpers.loginRequired, (req, res) => {
+//   knex('advices')
+//     .where('user_id', req.user.id)
+//     .then(advices => {
+//       res.render('myadvice', { advices: advices });
+//     });
+// });
