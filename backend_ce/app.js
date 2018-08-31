3aba9073e7b6bc1deed862497a72665fd62a3ba0
howellsuen <howell.suen@gmail.com>
2018-08-31 15:27:33 +0800

    socketIO fix on app.js

--- backend_ce/app.js
+++ backend_ce/app.js
@@ -8,10 +8,10 @@ const app = require('./utils/init-app')(knex);
 
 // Dependency Injection for Routers and Service
 const {
-  UserRouter,
-  ForumRouter,
-  QuestionRouter,
-  SocketIORouter
+	UserRouter,
+	ForumRouter,
+	QuestionRouter,
+	SocketIORouter
 } = require('./routers');
 
 const { UserService, ForumService, QuestionService } = require('./services');
@@ -20,20 +20,20 @@ let userService = new UserService(knex);
 let forumService = new ForumService(knex);
 let questionService = new QuestionService(knex);
 
-// new SocketIORouter(io).router();
 app.use('/api', new UserRouter(userService).router());
 app.use('/api/forum', new ForumRouter(forumService).router());
 app.use('/api/question', new QuestionRouter(questionService).router());
 
 // HTTP Setting
-
 const server = app.listen(8080, () => {
-  console.log('Server is running at port 8080');
+	console.log('Server is running at port 8080');
 });
 
-// const io = socketIO(server);
+// SocketIO Setting
+const io = socketIO(server);
+new SocketIORouter(io).router();
 
-// io.on('connection', socket => {
-//   console.log('client connected');
-//   socket.emit('Hello', { msg: 'Hello' });
-// });
+io.on('connection', socket => {
+	console.log('client connected');
+	socket.emit('Hello', { msg: 'Hello' });
+});
