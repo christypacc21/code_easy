d4698c4b2fc5ec90695c62ceb1c7f76c4f428291
Unknown <christypacc21@gmail.com>
2018-09-03 12:29:21 +0800

    forum frontend making getPosts
    WIP ( react component, forumAction) (figuring out how to do reducer)

--- frontend/src/pages/Forum/posts/PostCardList.js
+++ frontend/src/pages/Forum/posts/PostCardList.js
@@ -1,34 +1,72 @@
 // class component -edux
 import React, { Component } from 'react';
 import PostCard from './PostCard';
+import { connect } from 'react-redux';
 
 class PostCardList extends Component {
+  renderPostList() {
+    return this.props.postData.map((data, i) => {
+      return (
+        <div className="card col-sm-4">
+          <PostCard
+            key={i}
+            postid={data.posts[i].id}
+            propicPath={data.posts[i].profilePic}
+            username={data.posts[i].display_name}
+            dateTime={data.posts[i].created_at}
+            postTitle={data.posts[i].title}
+            postContent={data.posts[i].content}
+            postImagePath={data.posts[i].image_path}
+            count
+          />
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
       <div className="PostCardList">
-        <div className="row">
-          <div className="card col-sm-4">
-            <PostCard />
-          </div>
-          <div className="card col-sm-4">
-            <PostCard />
-          </div>
-          <div className="card col-sm-4">
-            <PostCard />
-          </div>
-          <div className="card col-sm-4">
-            <PostCard />
-          </div>
-          <div className="card col-sm-4">
-            <PostCard />
-          </div>
-          <div className="card col-sm-4">
-            <PostCard />
-          </div>
-        </div>
+        <div className="row">{this.renderPostList()}</div>
       </div>
     );
   }
 }
 
-export default PostCardList;
+function mapStateToProps(state) {
+  return {
+    postdata: state.postData
+  };
+}
+
+export default connect(mapStateToProps)(PostCardList);
+
+//   render() {
+//     return (
+//       <div className="PostCardList">
+//         <div className="row">
+//           <div className="card col-sm-4">
+//             <PostCard />
+//           </div>
+//           <div className="card col-sm-4">
+//             <PostCard />
+//           </div>
+//           <div className="card col-sm-4">
+//             <PostCard />
+//           </div>
+//           <div className="card col-sm-4">
+//             <PostCard />
+//           </div>
+//           <div className="card col-sm-4">
+//             <PostCard />
+//           </div>
+//           <div className="card col-sm-4">
+//             <PostCard />
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default PostCardList;
