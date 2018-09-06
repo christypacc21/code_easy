0a2ac99ddf3a1b39875ba959afd82e78f6eebad4
Unknown <christypacc21@gmail.com>
2018-09-06 15:10:16 +0800

    chatroom basic styling

--- frontend/src/pages/Chatroom/index.js
+++ frontend/src/pages/Chatroom/index.js
@@ -53,30 +53,40 @@ class Chatroom extends Component {
   render() {
     // console.log(this.props.match);
     return (
-      <div className="container">
-        Chat
-        {this.props.messages
-          .filter(
-            message => String(message.chatId) === this.props.match.params.chatId
-          )
-          .map((message, i) => {
-            console.log('message123', message);
-            return (
-              <div key={message.message + i}>
-                {message.displayName}: {message.message}
-              </div>
-            );
-          })}
-        <textarea
-          value={this.state.inputMessage}
-          onChange={e => this.setState({ inputMessage: e.target.value })}
-        />
-        <button className="button" onClick={this.sendMessage}>
-          Submit
-        </button>
-        <button className="button" onClick={this.endSession}>
-          End Session
-        </button>
+      <div className="jumbotron">
+        <div className="container">
+          Live Chat here!!!
+          <br />
+          {this.props.messages
+            .filter(
+              message =>
+                String(message.chatId) === this.props.match.params.chatId
+            )
+            .map((message, i) => {
+              console.log('message123', message);
+              return (
+                <div key={message.message + i}>
+                  {message.displayName}: {message.message}
+                </div>
+              );
+            })}
+          <textarea
+            className="form-control"
+            rows="1"
+            value={this.state.inputMessage}
+            onChange={e => this.setState({ inputMessage: e.target.value })}
+          />
+          <button
+            className="btn btn-info btn-lg btn-block"
+            onClick={this.sendMessage}
+          >
+            Submit
+          </button>
+          <br />
+          <button className="btn btn-warning btn-lg" onClick={this.endSession}>
+            End Session
+          </button>
+        </div>
       </div>
     );
   }
