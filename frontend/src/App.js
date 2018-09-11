2fb3c896200883e211676fb6d837cc8356e480c6
howellsuen <38110369+howellsuen@users.noreply.github.com>
2018-09-11 21:41:42 +0800

    change bootstrap from cdn to package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
