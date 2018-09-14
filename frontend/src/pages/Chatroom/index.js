996f9f076d759d9f8a973624e8e47fa25849fe06
Unknown <christypacc21@gmail.com>
2018-09-14 12:35:51 +0800

    added tab bar in chatroom
    next step: make the tab to be a smart one (toggle active by chatroom_active's value)

--- frontend/src/pages/Chatroom/index.js
+++ frontend/src/pages/Chatroom/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ChatTab from '../MyQuestions/ChatTab';
 import '../../App.css';
 
 import * as ChatActions from '../../redux/actions/chatActions';
@@ -56,46 +57,54 @@ class Chatroom extends Component {
     console.log(this.state.imputMessage);
     // console.log(this.props.match);
     return (
-      <div className="jumbotron">
-        <div className="container">
-          <h1>Live Chat!</h1>
-          <br />
-          {this.props.messages
-            .filter(
-              message =>
-                String(message.chatId) === this.props.match.params.chatId
-            )
-            .map((message, i) => {
-              console.log('chatroom message', message);
-              return (
-                <div key={i}>
-                  <strong>{message.displayName}: </strong>
-                  {message.role === 'instructor' ? (
-                    <pre className="instructorSpeechBubble">
-                      {message.message}
-                    </pre>
-                  ) : (
-                    <pre className="studentSpeechBubble">{message.message}</pre>
-                  )}
-                </div>
-              );
-            })}
-          <textarea
-            className="form-control"
-            rows="1"
-            value={this.state.inputMessage}
-            onChange={e => this.setState({ inputMessage: e.target.value })}
-          />
-          <button
-            className="btn btn-info btn-lg btn-block"
-            onClick={this.sendMessage}
-          >
-            Submit
-          </button>
-          <br />
-          <button className="btn btn-warning btn-lg" onClick={this.endSession}>
-            End Session
-          </button>
+      <div>
+        <ChatTab />
+        <div className="jumbotron">
+          <div className="container">
+            <h1>Live Chat!</h1>
+            <br />
+            {this.props.messages
+              .filter(
+                message =>
+                  String(message.chatId) === this.props.match.params.chatId
+              )
+              .map((message, i) => {
+                console.log('chatroom message', message);
+                return (
+                  <div key={i}>
+                    <strong>{message.displayName}: </strong>
+                    {message.role === 'instructor' ? (
+                      <pre className="instructorSpeechBubble">
+                        {message.message}
+                      </pre>
+                    ) : (
+                      <pre className="studentSpeechBubble">
+                        {message.message}
+                      </pre>
+                    )}
+                  </div>
+                );
+              })}
+            <textarea
+              className="form-control"
+              rows="1"
+              value={this.state.inputMessage}
+              onChange={e => this.setState({ inputMessage: e.target.value })}
+            />
+            <button
+              className="btn btn-info btn-lg btn-block"
+              onClick={this.sendMessage}
+            >
+              Submit
+            </button>
+            <br />
+            <button
+              className="btn btn-warning btn-lg"
+              onClick={this.endSession}
+            >
+              End Session
+            </button>
+          </div>
         </div>
       </div>
     );
