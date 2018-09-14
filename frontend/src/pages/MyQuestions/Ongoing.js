df96a4c59613c9c5755443ce5310bfad26d053a0
Unknown <christypacc21@gmail.com>
2018-09-14 11:47:19 +0800

    Koed Ongoing function

--- frontend/src/pages/MyQuestions/Ongoing.js
+++ frontend/src/pages/MyQuestions/Ongoing.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import OngoingList from './OngoingList';
 
 class Ongoing extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <ul className="nav nav-pills">
-          <li className="nav-item">
-            <Link className="nav-link active" to="/my-questions/ongoing">
-              On Going
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/my-questions/history">
-              History
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/my-questions/ongoing">
+                On Going
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/my-questions/history">
+                History
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <OngoingList />
+      </div>
     );
   }
 }
