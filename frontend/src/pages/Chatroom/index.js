fbfbd6fe8c539144c92615fc2de625cdb3ebd9bf
howellsuen <38110369+howellsuen@users.noreply.github.com>
2018-09-17 04:54:33 +0800

    Fix the reload chatroom problem
    Add a reload page and check whether the status state has changed before rendering

--- frontend/src/pages/Chatroom/index.js
+++ frontend/src/pages/Chatroom/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ReactLoading from 'react-loading';
 import ChatTab from '../MyQuestions/ChatTab';
 import InstructorInfoTab from './InstructorInfoTab';
 import '../../App.css';
@@ -15,12 +16,12 @@ import { getHistory } from '../../redux/actions/historyActions';
 class Chatroom extends Component {
   state = {
     inputMessage: '',
-    showInputBox: true
+    showInputBox: null
   };
 
-  // componentWillMount() {
-  //   this.props.getHistory();
-  // }
+  componentWillMount() {
+    this.props.getHistory();
+  }
 
   componentDidMount() {
     console.log('did-this.props', this.props);
@@ -38,7 +39,9 @@ class Chatroom extends Component {
       );
     }
     // this.props.getChatroomStatus(this.props.match.params.chatId);
+  }
 
+  componentWillReceiveProps() {
     // for hiding the input box by checking the chatroom status
     if (this.props.chatInfo) {
       console.log('this.props.chatInfo', this.props.chatInfo);
@@ -61,6 +64,8 @@ class Chatroom extends Component {
       return chatStatus.map(status => {
         if (status === false) {
           return this.setState({ showInputBox: false });
+        } else if (status === true) {
+          return this.setState({ showInputBox: true });
         } else {
           return null;
         }
@@ -107,74 +112,85 @@ class Chatroom extends Component {
     // this.props.instructorInfo
     //   ? console.log('this.props.instructorInfo', this.props.instructorInfo)
     //   : null;
-    // if (this.props.chatInfo) {
-    return (
-      <div>
-        <ChatTab />
-        {this.props.instructorInfo && this.props.user.role === 'student' ? (
-          <InstructorInfoTab details={this.props.instructorInfo} />
-        ) : null}
-        <div className="jumbotron" style={{ margin: 0 }}>
-          <div className="container">
-            <h1>Live Chat!</h1>
-            <br />
-            {this.props.messages
-              .filter(
-                message =>
-                  String(message.chatId) === this.props.match.params.chatId
-              )
-              .map((message, i) => {
-                console.log('chatroom message', message);
-                return (
-                  <div key={i}>
-                    <strong>{message.displayName}: </strong>
-                    {message.role === 'instructor' ? (
-                      <pre className="instructorSpeechBubble">
-                        {message.message}
-                      </pre>
-                    ) : (
-                      <pre className="studentSpeechBubble">
-                        {message.message}
-                      </pre>
-                    )}
-                  </div>
-                );
-              })}
-            {this.state.showInputBox ? (
-              <div>
-                <textarea
-                  className="form-control"
-                  rows="5"
-                  placeholder="type here..."
-                  value={this.state.inputMessage}
-                  onChange={e =>
-                    this.setState({ inputMessage: e.target.value })
-                  }
-                />
-                <button
-                  className="btn btn-info btn-lg btn-block"
-                  onClick={this.sendMessage}
-                >
-                  Send
-                </button>
-                <br />
-                <button
-                  className="btn btn-warning btn-lg"
-                  onClick={this.endSession}
-                >
-                  End Session
-                </button>
-              </div>
-            ) : (
-              <h2>This session has already expired.</h2> //if chatroom is no longer active, hide then input area.
-            )}
+    if (this.props.chatInfo && this.state.showInputBox !== null) {
+      return (
+        <div>
+          <ChatTab />
+          {this.props.instructorInfo && this.props.user.role === 'student' ? (
+            <InstructorInfoTab details={this.props.instructorInfo} />
+          ) : null}
+          <div className="jumbotron" style={{ margin: 0 }}>
+            <div className="container">
+              <h1>Live Chat!</h1>
+              <br />
+              {this.props.messages
+                .filter(
+                  message =>
+                    String(message.chatId) === this.props.match.params.chatId
+                )
+                .map((message, i) => {
+                  console.log('chatroom message', message);
+                  return (
+                    <div key={i}>
+                      <strong>{message.displayName}: </strong>
+                      {message.role === 'instructor' ? (
+                        <pre className="instructorSpeechBubble">
+                          {message.message}
+                        </pre>
+                      ) : (
+                        <pre className="studentSpeechBubble">
+                          {message.message}
+                        </pre>
+                      )}
+                    </div>
+                  );
+                })}
+              {this.state.showInputBox ? (
+                <div>
+                  <textarea
+                    className="form-control"
+                    rows="5"
+                    placeholder="type here..."
+                    value={this.state.inputMessage}
+                    onChange={e =>
+                      this.setState({ inputMessage: e.target.value })
+                    }
+                  />
+                  <button
+                    className="btn btn-info btn-lg btn-block"
+                    onClick={this.sendMessage}
+                  >
+                    Send
+                  </button>
+                  <br />
+                  <button
+                    className="btn btn-warning btn-lg"
+                    onClick={this.endSession}
+                  >
+                    End Session
+                  </button>
+                </div>
+              ) : (
+                <h2>This session has already expired.</h2> //if chatroom is no longer active, hide then input area.
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    );
-    // } else {
-    //   return null;
-    // }
+      );
+    } else {
+      return (
+        <div
+          className="loading"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <ReactLoading type="spin" color="#black" height={100} width={100} />
+        </div>
+      );
+    }
   }
 }
 
